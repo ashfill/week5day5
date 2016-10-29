dcfa44869394b4aa9292bdb91fb3745a9a3c22b2
ashfill <ashtonfillery@gmail.com>
2016-10-29 06:33:12 -0700

    MovieRental1

---
+++ MovieRental1/MovieRental1/Controllers/CustomerInfoesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MovieRental1;
+
+namespace MovieRental1.Controllers
+{
+    public class CustomerInfoesController : Controller
+    {
+        private MovieRentalEntities db = new MovieRentalEntities();
+
+        // GET: CustomerInfoes
+        public ActionResult Index()
+        {
+            return View(db.CustomerInfoes.ToList());
+        }
+
+        // GET: CustomerInfoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CustomerInfo customerInfo = db.CustomerInfoes.Find(id);
+            if (customerInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customerInfo);
+        }
+
+        // GET: CustomerInfoes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CustomerInfoes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CustomerID,CustomerPhoneNumber")] CustomerInfo customerInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CustomerInfoes.Add(customerInfo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(customerInfo);
+        }
+
+        // GET: CustomerInfoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CustomerInfo customerInfo = db.CustomerInfoes.Find(id);
+            if (customerInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customerInfo);
+        }
+
+        // POST: CustomerInfoes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CustomerID,CustomerPhoneNumber")] CustomerInfo customerInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(customerInfo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(customerInfo);
+        }
+
+        // GET: CustomerInfoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CustomerInfo customerInfo = db.CustomerInfoes.Find(id);
+            if (customerInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customerInfo);
+        }
+
+        // POST: CustomerInfoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CustomerInfo customerInfo = db.CustomerInfoes.Find(id);
+            db.CustomerInfoes.Remove(customerInfo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MovieRental1/MovieRental1/Controllers/RentalRecordsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MovieRental1;
+
+namespace MovieRental1.Controllers
+{
+    public class RentalRecordsController : Controller
+    {
+        private MovieRentalEntities db = new MovieRentalEntities();
+
+        // GET: RentalRecords
+        public ActionResult Index()
+        {
+            var rentalRecords = db.RentalRecords.Include(r => r.Movy);
+            return View(rentalRecords.ToList());
+        }
+
+        // GET: RentalRecords/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RentalRecord rentalRecord = db.RentalRecords.Find(id);
+            if (rentalRecord == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rentalRecord);
+        }
+
+        // GET: RentalRecords/Create
+        public ActionResult Create()
+        {
+            ViewBag.MovieID = new SelectList(db.Movies, "MovieID", "MovieName");
+            return View();
+        }
+
+        // POST: RentalRecords/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "RentalID,MovieID,CustomerID,RentalDate,DueDate,ReturnDate")] RentalRecord rentalRecord)
+        {
+            if (ModelState.IsValid)
+            {
+                db.RentalRecords.Add(rentalRecord);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MovieID = new SelectList(db.Movies, "MovieID", "MovieName", rentalRecord.MovieID);
+            return View(rentalRecord);
+        }
+
+        // GET: RentalRecords/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RentalRecord rentalRecord = db.RentalRecords.Find(id);
+            if (rentalRecord == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MovieID = new SelectList(db.Movies, "MovieID", "MovieName", rentalRecord.MovieID);
+            return View(rentalRecord);
+        }
+
+        // POST: RentalRecords/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "RentalID,MovieID,CustomerID,RentalDate,DueDate,ReturnDate")] RentalRecord rentalRecord)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(rentalRecord).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MovieID = new SelectList(db.Movies, "MovieID", "MovieName", rentalRecord.MovieID);
+            return View(rentalRecord);
+        }
+
+        // GET: RentalRecords/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RentalRecord rentalRecord = db.RentalRecords.Find(id);
+            if (rentalRecord == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rentalRecord);
+        }
+
+        // POST: RentalRecords/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            RentalRecord rentalRecord = db.RentalRecords.Find(id);
+            db.RentalRecords.Remove(rentalRecord);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
